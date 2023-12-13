99f18bc7142179f0a1392c380ac8d7e3c1e9a7a5
shivamt2708 <201b381@juetguna.in>
2023-12-13 21:28:24 +0530

    changing configuration

--- server/main.go
+++ server/main.go
@@ -1,51 +1,12 @@
 package main
 
-import (
-	"context"
-	"gofr.dev/pkg/gofr"
-	"go.mongodb.org/mongo-driver/bson"
-)
-
-type User struct {
-	email string `json:"email"`
-    username string `json:"username"`
-    password string `json:"password"`
-    role string `json:"role"`
-    location string `json:"location"`
-}
-type sign_response struct {
-	Success bool   `json:"success"`
-	Message string `json:"message"`
-}
-
-func SignupHandler(ctx *gofr.Context) (interface{}, error) {
-    email := ctx.PathParam("email")
-    username := ctx.PathParam("username")
-    password := ctx.PathParam("password")
-    role := ctx.PathParam("role")
-    location := ctx.PathParam("location")
-    // Connect to MongoDB
-
-    // Insert user data
-    collection := ctx.MongoDB.Collection("users")
-    collection.InsertOne(context.Background(), bson.M{
-        "email": email,
-        "username": username,
-        "password": password,
-        "role": role,
-        "location": location,
-    })
-
-    // Respond with success message
-    return true, nil
-}
+import "gofr.dev/pkg/gofr"
 
 func main() {
     app := gofr.New()
-    // Register your MongoDB-connected handlers
-    
-    app.POST("/signup/{email}/{username}/{password}/{role}/{location}", SignupHandler)
+    app.GET("/greet", func(ctx *gofr.Context) (interface{}, error) {
+        return "Hello World!", nil
+    })
 
-    // Start the server
     app.Start()
 }
